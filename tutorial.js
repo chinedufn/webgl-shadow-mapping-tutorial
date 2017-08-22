868778bf12cb7cae6dde9e59cf318236b67cd020
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-08-23 00:26:58 +1000

    Remove image to texture function

--- tutorial.js
+++ tutorial.js
@@ -431,35 +431,3 @@ function draw () {
   window.requestAnimationFrame(draw)
 }
 draw()
-
-
-function createImageFromTexture(gl, texture, width, height) {
-  // Create a framebuffer backed by the texture
-  var framebuffer = gl.createFramebuffer();
-  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-
-  // Read the contents of the framebuffer
-  var data = new Uint8Array(width * height * 4);
-  gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, data);
-
-  gl.deleteFramebuffer(framebuffer);
-
-  // Create a 2D canvas to store the result
-  var canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  var context = canvas.getContext('2d');
-
-  // Copy the pixels to a 2D canvas
-  var imageData = context.createImageData(width, height);
-  imageData.data.set(data);
-  context.putImageData(imageData, 0, 0);
-
-  var img = new Image();
-  img.src = canvas.toDataURL();
-  return img;
-}
-document.body.appendChild(
-  createImageFromTexture(gl, shadowDepthTexture, 1024, 1024)
-)
